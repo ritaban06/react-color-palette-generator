87e4f533732a95b19381f8761a657e2d97c91d7f
ritaban06 <ghoshritabancollege@gmail.com>
2024-09-22 09:37:15 +0530

    Modified tailwind css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-  darkMode: "class",
 }
